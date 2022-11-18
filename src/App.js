a2f4ed580619258400d9eeb56ac71e2b69bfe7df
RMP1992 <45944114+RMP1992@users.noreply.github.com>
2022-11-18 22:24:04 +0000

    hangman first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const letters = ["t", "o", "m", "b", "o", "l", "a"];
+  const [inputValue, setInputValue] = useState("");
+  const [userValue, setUserValue] = useState(false);
+  const [correctValue, setCorrectValue] = useState([]);
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    setInputValue(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    if (inputValue) {
+      let found = letters.filter((letter) => letter.includes(inputValue));
+      if (found.length > 0) {
+        console.log("Correct");
+        setUserValue(true);
+        setCorrectValue((correctValue) => [...correctValue, found]);
+        setInputValue("");
+      } else {
+        console.log("Incorrect");
+        setInputValue("");
+      }
+    }
+  };
+
+  const handleDisplay = () => {
+    for (let i = 0; i < correctValue; i++) {
+      return <h1>{correctValue[i]}</h1>;
+    }
+  };
+  const handleArray = () => {
+    if (userValue) {
+      let index = letters.indexOf(inputValue);
+      console.log("index: ", index);
+      if (index !== -1) {
+        letters.splice(index, 1);
+      }
+    }
+  };
+
+  console.log("correctValue: ", correctValue);
+  //   console.log("updated letters: ", letters);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input value={inputValue} onChange={handleChange} />
+      <button
+        onClick={() => {
+          handleSubmit();
+        }}
+      >
+        Submit
+      </button>
+      <div className="hangman">
+        {correctValue.map((letter, i) => (
+          <h2 key={i}>{letter}</h2>
+        ))}
+      </div>
     </div>
   );
 }
